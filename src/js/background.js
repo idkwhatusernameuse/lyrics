d54b5852e3df35ac78ea7ac70fc4cf8dd954840c
idkwhatusernameuse <idkwhatusernameuse@gmail.com>
2020-06-09 20:34:04 -0600

    Change background svg's attributes

--- src/js/background.js
+++ src/js/background.js
@@ -4,9 +4,9 @@ export function setColors(image) {
     Vibrant.from(image).getPalette(function(err, palette) {
         document.body.style.background = palette.DarkVibrant.getHex()
         console.log(palette)
-        document.querySelector('.blob1').childNodes[1].setAttribute('fill', palette.Vibrant.getHex())
-        document.querySelector('.blob2').childNodes[1].setAttribute('fill', palette.DarkMuted.getHex())
-        document.querySelector('.blob3').childNodes[1].setAttribute('fill', palette.Vibrant.getHex())
-        document.querySelector('.blob4').childNodes[1].setAttribute('fill', palette.Muted.getHex())
+        document.querySelector('#blob1').setAttribute('fill', palette.Vibrant.getHex())
+        document.querySelector('#blob2').setAttribute('fill', palette.DarkMuted.getHex())
+        document.querySelector('#blob3').setAttribute('fill', palette.Vibrant.getHex())
+        document.querySelector('#blob4').setAttribute('fill', palette.Muted.getHex())
     })
 }
